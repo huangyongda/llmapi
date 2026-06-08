6c640a2a98c9bd5ac3718c9709355cc15a830003
huangyongda <122402509@qq.com>
2026-06-09 02:01:46 +0800

    超过50k则算多一次用量

--- internal/handlers/response.go
+++ internal/handlers/response.go
@@ -190,32 +190,18 @@ func ResponseLogger() gin.HandlerFunc {
 		httpStatusCode := c.Writer.Status()
 		retry_num, _ := c.Get("retry_num")
 		requestID, _ := c.Get("RequestID")
-		gmlModel := c.GetBool("gmlModel")
 		userService := services.NewUserService()
-		useGlm, _ := c.Get("UseGlm")
 
-		beilv := 1.0
-		if gmlModel && ok && (post_model_name == "GLM-5.1" || post_model_name == "GLM-5-Turbo") {
-			//GLM-5.1和GLM-5-Turbo 14:00–18:00 (UTC+8) 扣除2倍 其他时间1倍
-			hour := time.Now().Hour()
-			beilv = 2.0
-			if hour >= 14 && hour <= 18 {
-				beilv = 3.0
+		if result.Usage.TotalTokens > 0 {
+			//decNum = result.Usage.TotalTokens 除以 50k，向上取整
+			decNum := int(float64(result.Usage.TotalTokens)/50000.0 + 0.9999)
+			if decNum > 1 {
+				_, _ = userService.CheckAndDecrementLimit(userId, strconv.Itoa(decNum-1))
 			}
-			// fmt.Println("GLM-5.1和GLM-5-Turbo 14:00–18:00 (UTC+8) 扣除", beilv, "倍", time.Now().Format("2006-01-02 15:04:05"))
-		}
-		if gmlModel {
-			beilv = beilv * 1.5
+
 		}
 
-		if useGlm == 1 && result.Usage.TotalTokens > 0 {
-			tokenUsage := result.Usage.TotalTokens
-			tokenUsage = int(float64(tokenUsage) * beilv)
-			// result.Usage.TotalTokens = tokenUsage
-			_, _ = userService.CheckAndDecrementLimit(userId, strconv.Itoa(tokenUsage))
-		}
-
-		fmt.Println("requestID:", requestID, ",model:", post_model_name, ",retry_num:", retry_num, ",httpStatusCode:", httpStatusCode, ",userId:", userId, ",keySuffix:", keySuffix, ",Time:", time.Now().Format("2006-01-02 15:04:05"), beilv, "倍", ",Current Usage:", useNum, ",llmResponse:", llmResponse)
+		fmt.Println("requestID:", requestID, ",model:", post_model_name, ",retry_num:", retry_num, ",httpStatusCode:", httpStatusCode, ",userId:", userId, ",keySuffix:", keySuffix, ",Time:", time.Now().Format("2006-01-02 15:04:05"), ",Current Usage:", useNum, ",llmResponse:", llmResponse)
 	}
 }
 
---
+++ internal/handlers/response_test.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestNumber(t *testing.T) {
+	TotalTokens := 163699
+	decNum := int(float64(TotalTokens)/50000.0 + 0.9999)
+	fmt.Println(decNum)
+	if decNum > 1 {
+		test := strconv.Itoa(decNum - 1)
+		fmt.Println(test)
+	}
+}
